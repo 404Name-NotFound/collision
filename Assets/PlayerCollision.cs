b423934dee60970681468e0934117eec4bda47c3
Yash Aggarwal <yashaggarwal85d@gmail.com>
2021-02-25 13:07:08 +0530

    vibration

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -1,9 +1,27 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerCollision : MonoBehaviour
 {
-    void OnTriggerEnter(Collider collider)
+    
+    public GameObject obj;
+
+    public float distanceBetweenObjects;
+
+    public void OnTriggerEnter(Collider collider)
     {
         Debug.Log(collider.GetComponent<Collider>().name);
-    }    
+    }
+        bool f=true;
+
+    private void Update()
+    {
+        obj = GameObject.Find("Straight_Terrain_WithRock/Rock_Straight");
+        distanceBetweenObjects = Vector3.Distance(transform.position, obj.transform.position);
+        if(distanceBetweenObjects < 5 & f){
+            Handheld.Vibrate();
+            f=false;
+        }
+        // Debug.DrawLine(transform.position, obj.transform.position, Color.green);
+    }
 }
